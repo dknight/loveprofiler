e79c71c65772e8c41a0787416ba5980cfdcb19c1
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2024-08-15 20:39:27 +0300

    fix: improved windows console support

--- drivers/console.lua
+++ drivers/console.lua
@@ -1,18 +1,38 @@
 -- LoveProfiler console driver implementation.
 
-local console = {}
-
-console.print = function(t, i)
-  -- Color not yet implemented. Maybe it is need to use dependency lcurses.
-  io.write(t._out[i].msg)
+---Checks that OS is Microsoft Windows.
+---@return boolean
+local function isWindows()
+	return type(package) == "table" and type(package.config) == "string" and package.config:sub(1, 1) == "\\"
 end
 
+---Checks that termianl supports colors.
+---@return boolean
+local function isColorSupported()
+	if isWindows() then
+		return not not os.getenv("ANSICON")
+	end
+	return true
+end
+
+---@class console
+local console = {}
+
+---@param t table
+---@param i number
+console.print = function(t, i)
+	-- Color not yet implemented. Maybe it is need to use dependency lcurses.
+	io.write(t._out[i].msg)
+end
+
+---@param t table
 console.flush = function(t)
-  t._out = {}
-  -- clear the terminal ANSI sequence. Should work on Windows if ANSY.SYS
-  -- is still supported on Windows.
-  io.write("\027[H\027[2J")
+	t._out = {}
+	-- clear the terminal ANSI sequence. Should work on Windows if ANSI.SYS
+	-- is still supported.
+	if isColorSupported() then
+		io.write("\027[H\027[2J")
+	end
 end
 
 return console
-
